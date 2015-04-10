c669d97ce60c449df4f89dc7e83cb178523270c4
suimasev <Susie@evaria.local>
2015-04-10 11:17:09 -0700

    Moved test setup and teardown into respective hooks

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 
 //Module inclusions           
 var testObj = require('./library/testUtils');       //Custom library to read config and data files
-var login = require('./library/loginPage');                 //Custom library to hold login information
+var login = require('./library/loginPage');         //Custom library to hold login information
 
 //Constants
 var CONFIG_FILE = "testConfig.json";        //Full path to test configuration
@@ -13,6 +13,7 @@ var TEST_DATA_FILE = "testData.json";       //Full path to test data
 var TIMEOUT = 15000;                        //Used to adjust script timeout
 
 var testInfo;                               //Imported test config and data
+var loginPage;                              //Instance of login page class
 
 /** Main test script function
 * @param {string} testDesc - Test suite description
@@ -22,15 +23,22 @@ function runTestScript(testDesc, testKey) {
     describe(testDesc, function(){
         this.timeout(TIMEOUT);   
 
+        before(function(){
+            //Create a new test object
+            loginPage = new login.loginPage(testInfo, testKey);             
+        })
+
+        after(function(){
+            //Close the browser
+            loginPage.close();
+        })
+
         //This is where the actual test steps take place
         it(testKey, function(done){ 
-            //Create a new test object
-            var loginPage = new login.loginPage(testInfo, testKey); 
 
             //Load the page and attempt to login with given credentials
             loginPage.loadPage();
             loginPage.attemptLogin();
-            loginPage.close();
 
             done();
         }); 
